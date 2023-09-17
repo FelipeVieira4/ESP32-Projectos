b26b1531a3bae4c79299d26c6114e8d05c55f6e9
FelipeVieira4 <felipevieira58238@gmail.com>
2023-09-17 14:20:29 -0300

    atualizado projeto fita-led

--- fita-led/fita-led.ino
+++ fita-led/fita-led.ino
@@ -1,41 +1,44 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        2 // On Trinket or Gemma, suggest changing this to 1
-
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 24 // Popular NeoPixel ring size
+#define PIN        2
+#define NUMPIXELS 24
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVAL 250 // Time (in milliseconds) to pause between pixels
+#define DELAYVAL 200
+
+uint8_t cores[] = {2,100,200};
+
+typedef struct{
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+}Cor;
 
 void setup() {
-
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  // END of Trinket-specific code.
-
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.begin();
 }
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
+  Cor corLed;
 
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-  // to the count of pixels minus one.
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+  for(int i=0; i<NUMPIXELS; i++) {
 
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(100, 0, 0));
+    if(i%4==0){
 
-    pixels.show();   // Send the updated pixel colors to the hardware.
+      //Redifinir as cores
+      corLed = (Cor){
+        cores[random(0,2)], //vermelho
+        cores[random(0,2)], //verde
+        cores[random(0,2)], //azul
+      };
+    }
 
-    delay(DELAYVAL); // Pause before next pass through loop
+    pixels.setPixelColor(i, pixels.Color(corLed.r,corLed.g,corLed.b));
+
+    pixels.show();
+
+    delay(DELAYVAL);
   }
+
 }
